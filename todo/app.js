27f41dc12bcd0286bf09ffb9695fdcc60adda939
Jack Franklin <jack@jackfranklin.net>
2015-10-05 12:02:15 +0100

    Basic Todo example

---
+++ todo/app.js
@@ -0,0 +1,32 @@
+class MyApp extends React.Component {
+  render() {
+    const todos = [
+      { id: 1, text: 'ReactJS workshop', done: false },
+      { id: 2, text: 'Go for a run', done: false },
+      { id: 3, text: 'Clean my bike', done: false },
+    ];
+
+    return (
+      <Todos todos={todos} />
+    );
+  }
+};
+
+class Todos extends React.Component {
+  render() {
+    const todoItems = this.props.todos.map((todo) => {
+      return (<li key={todo.id}>{todo.text}</li>);
+    });
+
+    return (
+      <ul>
+        { todoItems }
+      </ul>
+    );
+  }
+}
+
+React.render(
+  <MyApp />,
+  document.body
+);
